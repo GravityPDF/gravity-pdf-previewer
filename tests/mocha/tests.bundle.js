a7819de5e1627b3afb14366ccfce4c65948c87cb
Jake Jackson <jake@gravitypdf.com>
2017-07-24 13:27:56 +1000

    Add JS Unit test support

---
+++ tests/mocha/tests.bundle.js
@@ -0,0 +1,21 @@
+import $ from 'jquery'
+
+// setup global defaults that our tests expect is present
+window.PdfPreviewerConstants = {
+
+}
+
+// setup global before and after code
+beforeEach(function () {
+  $('body')
+    .append('<div id="karma-test-container">')
+})
+
+afterEach(function () {
+  $('#karma-test-container').remove()
+})
+
+// require all modules ending in ".test.js" from the
+// current directory and all subdirectories
+var testsContext = require.context(".", true, /.+\.test\.js?$/)
+testsContext.keys().forEach(testsContext)
