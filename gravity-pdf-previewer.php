074f836cdeb6eea237001517a658e6b3b4468f77
Jake Jackson <jake@gravitypdf.com>
2018-01-10 12:15:26 +1100

    Update version to 1.1

--- gravity-pdf-previewer.php
+++ gravity-pdf-previewer.php
@@ -8,7 +8,7 @@
  * Author URI:      https://gravitypdf.com
  * Text Domain:     gravity-pdf-previewer
  * Domain Path:     /languages
- * Version:         1.0.2
+ * Version:         1.1
  */
 
 /**
@@ -42,7 +42,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 */
 
 define( 'GFPDF_PDF_PREVIEWER_FILE', __FILE__ );
-define( 'GFPDF_PDF_PREVIEWER_VERSION', '1.0.2' );
+define( 'GFPDF_PDF_PREVIEWER_VERSION', '1.1.0' );
 
 /**
  * Class GPDF_Previewer_Checks
