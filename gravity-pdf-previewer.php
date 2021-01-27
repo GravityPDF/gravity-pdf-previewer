9068550edd6d1e3b316d8269e9c4bc4cf4fc3007
Jake Jackson <jake@gravitypdf.com>
2021-01-28 10:50:22 +1100

    Fix version number I forgot to change

--- gravity-pdf-previewer.php
+++ gravity-pdf-previewer.php
@@ -8,7 +8,7 @@
  * Author URI:      https://gravitypdf.com
  * Text Domain:     gravity-pdf-previewer
  * Domain Path:     /languages
- * Version:         1.3.1
+ * Version:         1.3.0
  */
 
 /**
